2966ac1295a83ee942136fe6b4744efd5c32321b
Jacob Bublitz <jacob@bublitz.com>
2020-08-09 19:24:08 -0700

    Add uvw as submodule

--- src/vega/server/main.cc
+++ src/vega/server/main.cc
@@ -1,27 +1,38 @@
 #include <iostream>
 
-#include "vega/net/tcp.h"
-#include "vega/server/client.h"
+#include "uvw.hpp"
+
+namespace {
+
+void UvwListen(uvw::Loop &loop) {
+  auto tcp = loop.resource<uvw::TCPHandle>();
+
+  tcp->once<uvw::ListenEvent>(
+      [](const uvw::ListenEvent &, uvw::TCPHandle &srv) {
+        auto cli = srv.loop().resource<uvw::TCPHandle>();
+
+        cli->on<uvw::DataEvent>([](const uvw::DataEvent &ev, uvw::TCPHandle &) {
+          std::cout.write(ev.data.get(), ev.length);
+        });
+
+        cli->on<uvw::CloseEvent>(
+            [ptr = srv.shared_from_this()](const uvw::CloseEvent &,
+                                           uvw::TCPHandle &) { ptr->close(); });
+        cli->on<uvw::EndEvent>(
+            [](const uvw::EndEvent &, uvw::TCPHandle &cli) { cli.close(); });
+
+        srv.accept(*cli);
+        cli->read();
+      });
+
+  tcp->bind("::", 25565);
+  tcp->listen();
+}
+
+}  // namespace
 
 int main() {
-  try {
-    WSAData wsa_data;
-    ::WSAStartup(MAKEWORD(2, 2), &wsa_data);
-
-    vega::net::TcpServer server;
-    server.Bind(25565);
-
-    server.Listen();
-    std::cout << "listening\n";
-
-    auto client = server.Accept();
-    std::cout << "Accepted connection\n";
-    vega::server::ClientConnection conn{std::move(client)};
-
-    while (true) {
-      conn.Poll();
-    }
-  } catch (std::exception e) {
-    std::cerr << e.what() << "\n";
-  }
+  auto loop = uvw::Loop::create();
+  UvwListen(*loop);
+  loop->run();
 }
